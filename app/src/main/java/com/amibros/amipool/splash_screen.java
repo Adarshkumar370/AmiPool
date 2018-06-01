4e5a9c6c6ccb8c803c615aefae742046d3995834
Adarsh Kumar <adarshkumar370@gmail.com>
2018-06-02 00:16:47 +0530

    Logo Animation 1.0

---
+++ app/src/main/java/com/amibros/amipool/splash_screen.java
@@ -0,0 +1,20 @@
+package com.amibros.amipool;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class splash_screen extends AppCompatActivity {
+    private ImageView logo;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.startup_screen);
+        logo=(ImageView) findViewById(R.id.Logo);
+        Animation a= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.custom_anim);
+        logo.startAnimation(a);
+
+    }
+}
