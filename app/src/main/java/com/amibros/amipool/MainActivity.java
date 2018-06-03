849f2a2e0b340efd64406779ea47fb04f0ad2315
Harshvardhan Sahay <harshvardhansahay5@gmail.com>
2018-06-03 17:11:47 +0530

    Minor Rename

--- app/src/main/java/com/amibros/amipool/MainActivity.java
+++ app/src/main/java/com/amibros/amipool/MainActivity.java
@@ -2,8 +2,6 @@ package com.amibros.amipool;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.startup_screen);
+        setContentView(R.layout.activity_main);
     }
 }
